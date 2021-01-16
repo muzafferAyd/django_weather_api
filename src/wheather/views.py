5922d508dc783a56c9ff7e2c489655f281db35f4
Muzaffer <aydogdum38@gmail.com>
2021-01-16 22:35:23 +0300

    add a city

--- src/wheather/views.py
+++ src/wheather/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from decouple import config
 import requests
 from pprint import pprint
 from .models import City
 from .forms import CityForm
+from django.contrib import messages
 
 def index(request):
     form = CityForm()
     cities = City.objects.all()
     url = config("BASE_URL")
+
+    if request.method == "POST":
+        form = CityForm(request.POST) #request.POST.get("name")
+        if form.is_valid():
+            new_city = form.cleaned_data["name"]
+            if not City.objects.filter(name=new_city).exists():
+                r = requests.get(url.format(new_city))
+                if r.status_code == 200:
+                    form.save()
+                    messages.success(request, "City added succesfully!")
+                else:
+                    messages.warning(request, "City does not exist.")
+            else:
+                messages.warning(request, "City already exists.")
+            return redirect("home")
+
 
     city_data = []
     for city in cities:
