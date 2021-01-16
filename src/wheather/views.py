229ebaf1c71cbfa631938fe17abdc45ec07f282e
Muzaffer <aydogdum38@gmail.com>
2021-01-16 15:40:27 +0300

    added models and city database

--- src/wheather/views.py
+++ src/wheather/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
+from .models import City
 
 def index(request):
-    return render(request, "wheather/index.html")
+    cities = City.objects.all()
+    url = config("BASE_URL")
+
+    city_data = []
+    for city in cities:
+        print(city)
+        r = requests.get(url.format(city))
+        content= r.json()  
+
+        weather_data = {
+        'city' : city.name,
+        'temp' : content['main']['temp'],
+        'description': content['weather'][0]["description"],
+        "icon" : content["weather"][0]["icon"]
+         }
+        city_data.append(weather_data)
+
+    context = {
+       "city_data": city_data
+    }
+    return render(request, "weather/index.html", context)
